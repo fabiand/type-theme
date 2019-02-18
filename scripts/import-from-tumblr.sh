9fc3b0cc8cb1beb75e20a141404919353cb292a5
Fabian Deutsch <fabiand@fedoraproject.org>
2019-02-18 15:32:34 +0100

    Reitroduce all posts
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- scripts/import-from-tumblr.sh
+++ scripts/import-from-tumblr.sh
@@ -1,7 +1,9 @@
 for F in $(ls -1 ../posts/ | grep .md ) ;
 do
   POSTID=${F%.md}
+#for POSTID in $(xmllint --xpath "//post/@id" ../posts/posts.xml  | egrep -o "[0-9]+") ;
+#do
   NEWFN=$(date --date="$(xmllint --xpath "string(tumblr/posts/post[@id=$POSTID]/@date)" ../posts/posts.xml )" +%Y-%m-%d-$POSTID.md)
   TITLE=$(xmllint --xpath "string(tumblr/posts/post[@id=$POSTID]/regular-title)" ../posts/posts.xml)
-  ( echo -e "---\nlayout: post\ntitle: \"${TITLE:-Today}\"\nauthor: fabiand\n---\n\n" ; cat ../posts/$F ) > ../_posts/$NEWFN
+  ( echo -e "---\nlayout: post\ntitle: \"${TITLE:-Today}\"\nauthor: fabiand\n---\n\n" ; [[ -n "$TITLE" ]]  && sed "/${TITLE//\//\\\/}/,+1 d" ../posts/$F || cat ../posts/$F ) > ../_posts/$NEWFN
 done
